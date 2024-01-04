b31aa2d72bc72d5d7add50a7d429f6a78a48948b
“Darkidd77” <m.almahdi77@yahoo.com>
2024-01-04 01:24:31 +0000

    task-1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ number = random.randint(-10000, 10000)
 lsdig = abs(number) % 10
 if number < 0:
     lsdig = -(lsdig)
-print(f"last digit of {number:d} is {lsdig:d} and is", end=" ")
+print(f"last digit of {number:d} is {lsdig:d} and is ", end="")
 
 if lsdig == 0:
     print("0")
