daab29e8b1cee903218ccdcc07b816ec29f0e661
“Darkidd77” <m.almahdi77@yahoo.com>
2024-01-04 23:43:27 +0000

    task-2-fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,11 @@ if __name__ == "__main__":
     import sys
 
     num = len(sys.argv) - 1
-    if num >= 1:
-        print("{} arguments:".format(num))
-        for i in range(num) :
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    if num == 1:
+        print("1 argument:")
     elif num == 0:
         print("0 arguments.")
+    else:
+        print("{} arguments:".format(num))
+    for i in range(num) :
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
