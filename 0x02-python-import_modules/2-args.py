7e0f06305e27f211daaeab24d2f5b278d37f422a
“Darkidd77” <m.almahdi77@yahoo.com>
2024-01-04 23:18:26 +0000

    task-2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    num = len(sys.argv) - 1
+
+    if num >= 1:
+        print("{} arguments:".format(num))
+        for i in range(num) :
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    elif num == 0:
+        print("0 arguements.")
