3cfd14c60149b603e97b255120cfa75c66c9248e
“Darkidd77” <m.almahdi77@yahoo.com>
2024-01-04 01:44:32 +0000

    fix-1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number < 10:
     lsdig = -(lsdig)
 print(f"last digit of {number:d} is {lsdig:d} and is ", end="")
 
-if lsdig >  5:
+if lsdig > 5:
     print("greater than 5")
 elif lsdig == 0:
     print("0")
