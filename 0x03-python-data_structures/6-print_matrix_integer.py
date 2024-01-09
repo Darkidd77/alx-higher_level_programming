7db309fc6646cd4854dcc4788cd78febcd4e508e
“Darkidd77” <m.almahdi77@yahoo.com>
2024-01-09 21:09:52 +0000

    task_fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for n in x:
-            print("{:d}".format(n), end = " " if n != x[-1] else "")
+            print("{:d}".format(n), end=" " if n != x[-1] else "")
         print()
