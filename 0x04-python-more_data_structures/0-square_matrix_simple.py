6169661a0702f8c160015c89457b31a09221e50c
“Darkidd77” <m.almahdi77@yahoo.com>
2024-01-10 17:50:48 +0000

    task-0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     x_matrix = matrix
     for n in range(len(matrix)):
         x_matrix[n] = list(map(lambda x: x**2, matrix[n]))
-    return x_matrix
+        return x_matrix
