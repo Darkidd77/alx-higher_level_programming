be5abdb2166303c1b25cfb6459e07f297bf65c79
“Darkidd77” <m.almahdi77@yahoo.com>
2024-01-22 20:06:59 +0000

    task0-fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,10 @@ def safe_print_list(my_list=[], x=0):
 
     for a in range(x):
         try:
-            print(my_list[a], end="")
-            a += 1
+            while a is not x:
+                print(my_list[a], end="")
+                a += 1
         except IndexError:
-            break
+            None
         print()
         return a
