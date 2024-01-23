4e8b4bc6740cbe3d13aa64982663cb09939cbd21
“Darkidd77” <m.almahdi77@yahoo.com>
2024-01-23 19:53:30 +0000

    task-1-fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,3 +1,10 @@
 class Square:
+    '''Define a class: Square'''
+
     def __init__(self, size):
+        '''init.
+
+        Args:
+            size: square size.
+        '''
         self.__size = size
