773de4c93155be985fbe19e7c7be1a27156f2b1c
“Darkidd77” <m.almahdi77@yahoo.com>
2024-04-20 00:57:55 +0100

    task-o

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,19 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database sorted in asc. order by states.id
-"""
-import MySQLdb
+"""Module lists states from mySQL database"""
 import sys
+import MySQLdb
 
-
-if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    try:
-        conndb = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=db_name,
-            charset="utf8"
-        )
-    except MySQLdb.Error as e:
-        print("Error connecting to database: {}".format(e))
-        sys.exit(1)
-
-    cu = conndb.cursor()
-    cu.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cu.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cu.close()
-    conndb.close()
+def list_states (username, password, database):
+    """lists states from database.
+    Ags:
+        username: mysql-username
+        password: mysql-password
+        database: mysql-database
+    """
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host='localhost',\
+            port=3306,\
+            user=username,\
+            passwd=password,\
+            db=database)
+    cursor = db.cursor()
