ca3c732b8d843563a9b5e7ea1ef056653e546dcf
“Darkidd77” <m.almahdi77@yahoo.com>
2024-04-27 21:56:04 +0100

    task-0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl to fetch the URL and capture response body size in bytes
-echo "SIZE=$(curl -s -o /dev/null -w '%{size_download}' "$URL")"
+echo SIZE=$(curl -s -o /dev/null -w '%{size_download}' "$URL")
