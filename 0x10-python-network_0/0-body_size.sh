9cd47346319460c10c5a70484ab835a092beb9e8
“Darkidd77” <m.almahdi77@yahoo.com>
2024-04-27 21:54:04 +0100

    task-0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL=$1
-
 # curl to fetch the URL and capture response body size in bytes
-# -w '%{size_download}': Print the size of response
-SIZE=$(curl -s -o /dev/null -w '%{size_download}' "$URL")
-
-echo "$SIZE"
+echo "SIZE=$(curl -s -o /dev/null -w '%{size_download}' "$URL")"
