ceaa9d2eb3ee7b8b1d9f7da306741f573a53af50
Darkidd77 <m.almahdi77@yahoo.com>
2024-04-30 17:28:20 -0700

    task-1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # GET request to the URL, and displays the body of the response
-curl -sL -o /dev/null -w "%{http_code}\n"  "$1" | grep 200 && curl -s "$1"
+curl -sSfL --location --write-out "%{http_code}\n"  "$1" | grep -E '^200$' && curl -sSf "$1"
