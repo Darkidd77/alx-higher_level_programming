3a2243c18b58fd75f2e11639d7ac82ad6f28f840
“Darkidd77” <m.almahdi77@yahoo.com>
2024-06-03 23:39:14 +0100

    task-all-fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,8 @@
 import sys
 import MySQLdb
 
-def list_states (username, password, database):
+
+def list_states(username, password, database):
     """lists states from database.
     Ags:
         username: mysql-username
@@ -11,9 +12,32 @@ def list_states (username, password, database):
         database: mysql-database
     """
     # Connect to the MySQL server
-    db = MySQLdb.connect(host='localhost',\
-            port=3306,\
-            user=username,\
-            passwd=password,\
+    db = MySQLdb.connect(host='localhost', \
+            port=3306, \
+            user=username, \
+            passwd=password, \
             db=database)
     cursor = db.cursor()
+
+    # Execute the SQL query to fetch all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows from the query result
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the database connection
+    db.close()
+
+
+# Example usage
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
